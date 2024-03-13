4d6011fd173d68a6160f23b6f8260370ddedb386
Nathan Roark <nathanroark@protonmail.com>
2024-03-12 23:18:50 -0500

    not awful

--- src/lib/components/voice-graph.svelte
+++ src/lib/components/voice-graph.svelte
@@ -17,24 +17,24 @@
     sampleRate: number
   ) {
     // Perform a quick root-mean-square to see if we have enough signal
-    var SIZE = buffer.length
-    var sumOfSquares = 0
-    for (var i = 0; i < SIZE; i++) {
-      var val = buffer[i]
+    let SIZE = buffer.length
+    let sumOfSquares = 0
+    for (let i = 0; i < SIZE; i++) {
+      let val = buffer[i]
       sumOfSquares += val * val
     }
-    var rootMeanSquare = Math.sqrt(sumOfSquares / SIZE)
+    let rootMeanSquare = Math.sqrt(sumOfSquares / SIZE)
     if (rootMeanSquare < 0.01) {
       return -1
     }
 
     // Find a range in the buffer where the values are below a given threshold.
-    var r1 = 0
-    var r2 = SIZE - 1
-    var threshold = 0.2
+    let r1 = 0
+    let r2 = SIZE - 1
+    let threshold = 0.2
 
     // Walk up for r1
-    for (var i = 0; i < SIZE / 2; i++) {
+    for (let i = 0; i < SIZE / 2; i++) {
       if (Math.abs(buffer[i]) < threshold) {
         r1 = i
         break
@@ -42,7 +42,7 @@
     }
 
     // Walk down for r2
-    for (var i = 1; i < SIZE / 2; i++) {
+    for (let i = 1; i < SIZE / 2; i++) {
       if (Math.abs(buffer[SIZE - i]) < threshold) {
         r2 = SIZE - i
         break
@@ -54,7 +54,7 @@
     SIZE = buffer.length
 
     // Create a new array of the sums of offsets to do the autocorrelation
-    var c = new Array(SIZE).fill(0)
+    let c = new Array(SIZE).fill(0)
     // For each potential offset, calculate the sum of each buffer value times its offset value
     for (let i = 0; i < SIZE; i++) {
       for (let j = 0; j < SIZE - i; j++) {
@@ -63,34 +63,34 @@
     }
 
     // Find the last index where that value is greater than the next one (the dip)
-    var d = 0
+    let d = 0
     while (c[d] > c[d + 1]) {
       d++
     }
 
     // Iterate from that index through the end and find the maximum sum
-    var maxValue = -1
-    var maxIndex = -1
-    for (var i = d; i < SIZE; i++) {
+    let maxValue = -1
+    let maxIndex = -1
+    for (let i = d; i < SIZE; i++) {
       if (c[i] > maxValue) {
         maxValue = c[i]
         maxIndex = i
       }
     }
 
-    var T0 = maxIndex
+    let T0 = maxIndex
 
     // Not as sure about this part, don't @ me
     // From the original author:
     // interpolation is parabolic interpolation. It helps with precision. We suppose that a parabola pass through the
     // three points that comprise the peak. 'a' and 'b' are the unknowns from the linear equation system and b/(2a) is
     // the "error" in the abscissa. Well x1,x2,x3 should be y1,y2,y3 because they are the ordinates.
-    var x1 = c[T0 - 1]
-    var x2 = c[T0]
-    var x3 = c[T0 + 1]
+    let x1 = c[T0 - 1]
+    let x2 = c[T0]
+    let x3 = c[T0 + 1]
 
-    var a = (x1 + x3 - 2 * x2) / 2
-    var b = (x3 - x1) / 2
+    let a = (x1 + x3 - 2 * x2) / 2
+    let b = (x3 - x1) / 2
     if (a) {
       T0 = T0 - b / (2 * a)
     }
@@ -113,7 +113,7 @@
       const bufferLength = analyser.fftSize
       const dataArray = new Float32Array(bufferLength)
 
-      const maxPitchThreshold = 2000 // Define the maximum valid pitch threshold
+      const maxPitchThreshold = 200 // Define the maximum valid pitch threshold
 
       const getAudioData = () => {
         analyser.getFloatTimeDomainData(dataArray)
@@ -150,11 +150,12 @@
 
     const yScale = d3
       .scaleLinear()
-      .domain([0, 1000]) // hardcode pitch range for visualization
+      .domain([0, 800]) // hardcode pitch range for visualization
       .range([height, 0])
 
     const line = d3
       .line<DataItem>()
+      .defined((d) => d.pitch > -1) // Only include points with pitch > -1
       .x((d) => xScale(d.time))
       .y((d) => yScale(d.pitch))
       .curve(d3.curveLinear)
