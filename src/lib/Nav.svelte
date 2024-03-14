7bb792cac891cb5593a8f689d238afc12535bd5c
ArcherScript <mikearche@gmail.com>
2024-03-14 16:36:31 -0700

    💄 Redesign header

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,8 +1,17 @@
 <script>
-  import { Heart } from "lucide-svelte";
+  import { Heart, ListMusic, Waves } from "lucide-svelte";
   import TransFlag from "./TransFlag.svelte";
   import * as Tooltip from "$lib/components/ui/tooltip";
   import Button from "./components/ui/button/button.svelte";
+
+  import { onMount } from "svelte";
+  import { cn } from "./utils";
+
+  let currentPath = "";
+
+  onMount(() => {
+    currentPath = window.location.pathname;
+  });
 </script>
 
 <header
@@ -11,13 +20,33 @@
   <div class="flex gap-4 items-center">
     <TransFlag class="w-6 h-6" />
     <h1 class="font-bold text-lg">Trans Voice</h1>
+    <a
+      class={cn(
+        "flex transition-colors hover:text-foreground/80 text-foreground/60 flex-row gap-2",
+        currentPath === "/" && "text-foreground/80"
+      )}
+      href="/"
+    >
+      <Waves /> Pitch
+    </a>
+    <a
+      class={cn(
+        "flex transition-colors hover:text-foreground/80 text-foreground/60 flex-row gap-2",
+        currentPath === "/tone" && "text-foreground/80"
+      )}
+      href="/tone"
+    >
+      <ListMusic /> Tone
+    </a>
   </div>
   <Tooltip.Root>
     <Tooltip.Trigger>
-      <Button variant="ghost"><Heart class="size-4 stroke-[#F7A8B8]" /></Button>
+      <Button variant="ghost" class="flex flex-row gap-2"
+        >Support us!<Heart class="size-4 stroke-[#F7A8B8]" /></Button
+      >
     </Tooltip.Trigger>
     <Tooltip.Content>
-      <p>Sponsor us!</p>
+      <p>Support us!</p>
     </Tooltip.Content>
   </Tooltip.Root>
 </header>
